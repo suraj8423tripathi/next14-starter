094c80f3569537e2e10d1fa6a4fefe8b5cd565c9
Suraj <suraj8423tripathi@gmail.com>
2024-03-01 03:20:52 +0530

    basic project setting

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LogInPage = () => {
+  return <div>Log in Page</div>;
+};
+
+export default LogInPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
-const Home = () => {
-  return <div>Hello World!</div>;
+const HomePage = () => {
+  return <div>Home Page</div>;
 };
 
-export default Home;
+export default HomePage;
